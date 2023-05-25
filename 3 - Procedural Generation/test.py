9e478e700e5b405aaeef915995081694474db680
Anand Natu <natu.anand@gmail.com>
2023-05-24 17:34:24 -0700

    Created folder and found working example for procedural environment generation

---
+++ 3 - Procedural Generation/test.py
@@ -0,0 +1,9 @@
+# Test file for procedural generation tooling
+# (Eventually need to figure out implementations in C# for Unity integration, or figure out how to connect Unity to Python code)
+
+
+## First let's do a simple text example on images to figure out the basic logic
+# https://towardsdatascience.com/replicating-minecraft-world-generation-in-python-1b491bc9b9a4?gi=ef52195dc0a5
+
+
+
